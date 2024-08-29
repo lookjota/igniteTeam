9486e1f8cd8e2573c6802fb3f12d42e5e5b3832d
lookjota <lookjota@gmail.com>
2024-08-29 15:56:12 -0300

    Definindo o tema da aplicacao

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 
+import { ThemeProvider } from 'styled-components';
 import Groups from '../screens/Groups';
+
+import theme from '../theme'; 
 
 export default function TabOneScreen() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups/>
-    </>
+    </ThemeProvider>
   );
 }
 
