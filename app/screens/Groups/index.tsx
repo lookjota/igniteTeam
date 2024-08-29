be7ad3eb5ca362024466feb138ee8c6d1fe2a16c
lookjota <lookjota@gmail.com>
2024-08-29 15:53:37 -0300

    utilizando styled components

--- app/screens/Groups/index.tsx
+++ app/screens/Groups/index.tsx
@@ -1,30 +1,13 @@
-import { StyleSheet, Text } from 'react-native';
 
-import { View } from '@/components/Themed';
+import { Container, Title } from './styles';
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
+    <Container>
+      <Title>
+        Hello World
+      </Title>
 
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
